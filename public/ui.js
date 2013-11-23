1b9c5ef68a44268cfd91b44a3d240a7151f4b1d7
Christoph Hüsler <christoph.huesler@burnstone.ch>
2013-11-23 13:59:16 +0100

    Initial version

---
+++ public/ui.js
@@ -0,0 +1,20 @@
+﻿(function($) {
+
+	var templates = {};
+
+	templates.links = doT.template($("#template-link").text());
+
+	jQuery.ajax({
+		type:"get", 
+		contentType: "application/json; charset=utf-8", 
+		url: "entries/", 
+		dataType: "json", 
+		success: function(data){
+			$.each(data, function(entry) {
+				$("#links").append(templates.links(entry));
+			});
+		},
+		error: function(e) { throw e; }
+	});
+   
+})(jQuery);
