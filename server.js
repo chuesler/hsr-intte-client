6c895f5a668a226e2957cd33ce4eb7be9376c5da
Christoph Hüsler <christoph.huesler@burnstone.ch>
2013-11-26 13:32:13 +0100

    add logging to server

--- server.js
+++ server.js
@@ -3,16 +3,17 @@ var http = require('http');
 var io = require('socket.io');
 var less = require('less');
 var fs = require('fs');
+var console = require('console');
 
 var User = require('./user.js');
 var Link = require('./link.js');
 var Comment = require('./comment.js');
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+   res.header('Access-Control-Allow-Headers', 'Content-Type');
+   next();
 };
 
 var app = express();
@@ -26,29 +27,30 @@ app.use(app.router);
 var clientCssFile = 'client.less';
 var clientCss = '';
 function renderCss(){
-    fs.readFile(clientCssFile, { 'encoding': 'utf-8' }, function(err, data){
-        if (err) throw err;
-        less.render(data, function(e, css){
-            if (e) throw e;
-            clientCss = css;
-        });
-    });    
+  fs.readFile(clientCssFile, { 'encoding': 'utf-8' }, function(err, data){
+    if (err) throw err;
+    less.render(data, function(e, css){
+      if (e) throw e;
+      clientCss = css;
+   });
+ });    
 }
 renderCss();
 fs.watch('.', function(event, filename){ // re-render css if it changes
     // watch directory instead of file because of editors using tempfile -> rename for atomicity (watch works on inodes on linux)
     if (filename === clientCssFile){
-        renderCss();
-    }
+      renderCss();
+   }
 });
 
+var log = console.log;
 
 function checkAuth(req, res, next) {
-    if (typeof(req.session.user_id) == "number") {
-        next();
-    } else {
-        res.send('You are not authorized!');
-    }
+  if (typeof(req.session.user_id) == "number") {
+    next();
+ } else {
+    res.send('You are not authorized!');
+ }
 }
 
 var entries = [];
@@ -61,141 +63,168 @@ var comment = new Comment(0, "TestComment", "Author");
 comments.push(comment);
 entries[0].comments.push(comment);
 
+entries.push(new Link(entries.length, "Title 2", "Author 2", "http://www.heise.de"));
+
 //default user
 users.push(new User(users.length, "a", "a") );
-  
+
 function findUser(name)
 {
 	for (var i in users) 
 	{
-	   var user = users[i];
-	   if( user.name == name)
-	   {
-		   return user;
-	   }
-	}
-	return null;
+    var user = users[i];
+    if( user.name == name)
+    {
+     return user;
+  }
+}
+return null;
 }
 
 function returnIndex(res, id, array) {
-    if (array.length <= id || id < 0) {
-        res.statusCode = 404;
-        return res.send('Error 404: No entry found');
-    }
-    return res.json(array[id]);
+  if (array.length <= id || id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No entry found');
+ }
+ return res.json(array[id]);
 }
 
 app.get('/', function(req, res) {
-  res.type('text/plain'); 
-  res.json(entries);
+  // res.type('text/plain'); 
+  // res.json(entries);
+  res.redirect('client.html');
 });
 
 app.get('/client.css', function(req, res){
-    res.type('text/css');
-    res.send(clientCss);
+   res.type('text/css');
+   res.send(clientCss);
 });
- 
+
 app.get('/login', function (req, res) {
-    if (typeof (req.session.user_id) == "number") {
-        res.json(users[req.session.user_id].name);
-        return;
-    }
-    res.json("");
-});
- 
- app.post('/login', function (req, res) {
-    var post = req.body;  
-	var user = findUser(post.name);	 
-	if( !!user && post.password == user.password)
-	{		
-		req.session.user_id = user.id;		
-		res.json(true);		
-		return;
-	}	
-	res.json(false);
+   if (typeof (req.session.user_id) == "number") {
+      var username = users[req.session.user_id].name
+      res.json(username);
+      log("get login: currently logged in: " + username);
+      return;
+   }
+   log("get login: not logged in");
+   res.json("");
 });
 
- app.post('/register', function(req, res) {
-     var post = req.body;
-     
-     if (typeof(post.name) != "string" || typeof(post.password) != "string") {
-         res.json(false);
-         return;
-     }
-     
-     if (findUser(post.name)) {
-         res.json(false);
-         return;
-     }
-     users.push(new User(users.length, post.name, post.password));
-     res.json(true);
- });
- 
- app.get('/users', function (req, res) {
-     res.json(users);
- });
+app.post('/login', function (req, res) {
+   var post = req.body;  
+   var user = findUser(post.name);	 
 
- 
- 
- app.get('/entries', function (req, res) {
-    res.json(entries);
+   if( !!user && post.password == user.password)
+   {		
+      log("post login: login succeeded: " + post.name);
+      req.session.user_id = user.id;		
+      res.json(true);		
+      return;
+   }	
+   log("post login: login failed: " + post.name);
+   res.json(false);
+});
+
+app.post('/register', function(req, res) {
+   var post = req.body;
+
+   if (typeof(post.name) != "string" || typeof(post.password) != "string") {
+      log("post register: invalid request");
+      res.json(false);
+      return;
+   }
+
+   if (findUser(post.name)) {
+      log("post register: user " + post.name + " is already registered");
+      res.json(false);
+      return;
+   }
+
+   log("post register: user " + post.name + " registered");
+
+   users.push(new User(users.length, post.name, post.password));
+   res.json(true);
+});
+
+app.get('/users', function (req, res) {
+   log("get users: " + users.length);
+   res.json(users);
+});
+
+
+
+app.get('/entries', function (req, res) {
+   log("get entries: " + entries.length);
+   res.json(entries);
 });
 
 
 app.post('/entry', function(req, res) {
-    var newLink = new Link(entries.length, req.body.title, users[req.session.user_id].name, req.body.url);	
- 	entries.push(newLink);
- 	res.json(newLink);
- 	io.sockets.emit('message', { action: "AddLink" });
+   var newLink = new Link(entries.length, req.body.title, users[req.session.user_id].name, req.body.url);	
+
+   log("post entry: " + req.body.title);
+
+   entries.push(newLink);
+   res.json(newLink);
+   io.sockets.emit('message', { action: "AddLink" });
 });
 
 app.get('/entry/:id', function(req, res) {
+   log("get entry/" + req.params.id);
    returnIndex(res,  req.params.id, entries);
 });
 
 app.post('/entry/:id/up', checkAuth, function (req, res) {
-    res.json(entries[req.params.id].rating._up(req.session.user_id));
-    io.sockets.emit('message', { action: "Rated" });
+   log("post entry/" + req.params.id + "/up");
+   res.json(entries[req.params.id].rating._up(req.session.user_id));
+   io.sockets.emit('message', { action: "Rated" });
 });
 
 app.post('/entry/:id/down', checkAuth, function (req, res) {
-    res.json(entries[req.params.id].rating._down(req.session.user_id));
-    io.sockets.emit('message', { action: "Rated" });
+   log("post entry/" + req.params.id + "/down");
+   res.json(entries[req.params.id].rating._down(req.session.user_id));
+   io.sockets.emit('message', { action: "Rated" });
 });
 
 app.post('/entry/:id/comment', checkAuth, function (req, res) {
-    var newComment = new Comment(comments.length, req.body.text, users[req.session.user_id].name);
-    comments.push(newComment);
+   log("post entry/" + req.params.id + "/comment");
+   var newComment = new Comment(comments.length, req.body.text, users[req.session.user_id].name);
+   comments.push(newComment);
 
-    var entry = entries[req.params.id];
-    entry.comments.push(newComment);
-    res.json(newComment);
-    io.sockets.emit('message', { action: "AddComment" });
+   var entry = entries[req.params.id];
+   entry.comments.push(newComment);
+   res.json(newComment);
+   io.sockets.emit('message', { action: "AddComment" });
 });
 
 app.post('/comment/:id/', checkAuth, function (req, res) {
-    var newComment = new Comment(comments.length, req.body.text, users[req.session.user_id].name);
-    comments.push(newComment);
+   log("post comment/" + req.params.id);
+   var newComment = new Comment(comments.length, req.body.text, users[req.session.user_id].name);
+   comments.push(newComment);
 
-    var comment = comments[req.params.id];
-    comment.comments.push(newComment);
-    res.json(newComment);
-    io.sockets.emit('message', { action: "AddComment" });
+   var comment = comments[req.params.id];
+   comment.comments.push(newComment);
+   res.json(newComment);
+   io.sockets.emit('message', { action: "AddComment" });
 });
 
 app.post('/comment/:id/up', checkAuth, function (req, res) {
-    res.json(comments[req.params.id].rating._up(req.session.user_id));
-    io.sockets.emit('message', { action: "Rated" });
+   log("post comment/" + req.params.id + "/up");
+   res.json(comments[req.params.id].rating._up(req.session.user_id));
+   io.sockets.emit('message', { action: "Rated" });
 });
 
 app.post('/comment/:id/down', checkAuth, function (req, res) {
-    res.json(comments[req.params.id].rating._down(req.session.user_id));
-    io.sockets.emit('message', { action: "Rated" });
+   log("post comment/" + req.params.id + "/down");
+   res.json(comments[req.params.id].rating._down(req.session.user_id));
+   io.sockets.emit('message', { action: "Rated" });
 });
 
 app.post('/logout', function (req, res) {
-	req.session.user_id  = null;	
-	res.json(true);
+   log("post logout");
+   req.session.user_id  = null;	
+   res.json(true);
 });
 
 app.use('/', express.static(__dirname + '/public/'));
@@ -204,11 +233,11 @@ app.use('/', express.static(__dirname + '/public/'));
 io = io.listen(app.listen(process.env.PORT || 4730));
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', { action: 'connected' });
+  socket.emit('message', { action: 'connected' });
 });
 
 io.sockets.on('disconnect', function (socket) {
-    socket.emit('message', { action: 'disconnect' });
+  socket.emit('message', { action: 'disconnect' });
 });
 
 
